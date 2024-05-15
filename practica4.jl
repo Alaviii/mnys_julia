84c91758a76444071937b6d461e329dbed11ff17
Alavii <alexravi85@gmail.com>
2024-05-15 16:19:23 +0200

    last stretch

---
+++ practica4.jl
@@ -0,0 +1,17 @@
+using DelimitedFiles
+
+function main()
+    function exmasx(x)
+        return ℯ^x + x
+    end
+    function polinomiomalvado(x)
+        return x^3-5x^2+7x-3
+    end
+
+    
+end
+
+function biseccion(f::Function)
+
+
+main()
